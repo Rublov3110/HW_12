7e8a4a9c72b8bc225cec586bf2fe47c076e2ef28
Rublov Viacheslav <timmy31101994@gmail.com>
2022-06-09 15:13:37 +0300

    HW_12 Start

---
+++ Module3HW2/Configs/DIConfig.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace HW_12
+{
+    public class DIConfig
+    {
+        public IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Starter>();
+            builder.RegisterType<AlphabetConfigurationService>().As<IAlphabetConfigurationService>();
+
+            var container = builder.Build();
+
+            return container;
+        }
+    }
+}
---
+++ Module3HW2/Interfaces/IAlphabetConfigurationService.cs
@@ -0,0 +1,8 @@
+﻿namespace HW_12
+{
+    public interface IAlphabetConfigurationService
+    {
+        public void SerializeAlphabetConfig(AlphabetConfig alphabetConfig, string jsonFilePath);
+        public AlphabetConfig DeserializeAlphabetConfig(string jsonFilePath);
+    }
+}
